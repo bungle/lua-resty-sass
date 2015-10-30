bb81344d0cef14bda3d6969bd81dfcf934f48932
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2015-10-30 02:52:41 +0200

    Added fallback to sha1 hash.

--- lib/resty/sass/modified.lua
+++ lib/resty/sass/modified.lua
@@ -1,14 +1,32 @@
+local io = io
+local open = io.open
 local ok, lfs = pcall(require, "syscall.lfs")
 if not ok then
     ok, lfs = pcall(require, "lfs")
 end
-assert(ok, "Either syscall or lfs is required.")
-local att = lfs.attributes
+ok = false
+local mod
+if ok then
+    local att = lfs.attributes
+    mod = function(file)
+        return att(file, "modification")
+    end
+else
+    local sha1 = ngx.sha1_bin
+    mod = function(file)
+        local f = open(file)
+        if not f then return nil end
+        local d = f:read "*a"
+        f:close()
+        if not d then return nil end
+        return sha1(d)
+    end
+end
 local tim = {}
 return function(file)
-    local lm = att(file, "modification")
+    local lm = mod(file)
     if lm == nil then return nil end
     local ts = tim[file] or 0
     tim[file] = lm
-    return lm > ts
+    return lm ~= ts
 end
